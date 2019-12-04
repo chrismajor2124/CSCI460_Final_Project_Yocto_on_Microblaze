23b74950798455259a5650a28221b16c95780600
chrismajor2124 <chrismichelmajor@hotmail.com>
2019-12-04 16:24:26 -0700

    MCS file for Petalinux created and tested

--- arty_linux.runs/impl_1/arty_microblaze_01_wrapper.tcl
+++ arty_linux.runs/impl_1/arty_microblaze_01_wrapper.tcl
@@ -61,109 +61,17 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param chipscope.maxJobs 2
-  create_project -in_memory -part xc7a35ticsg324-1L
-  set_property board_part digilentinc.com:arty-a7-35:part0:1.0 [current_project]
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.cache/wt [current_project]
-  set_property parent.project_path /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.xpr [current_project]
-  set_property ip_output_repo /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  set_property XPM_LIBRARIES {XPM_CDC XPM_FIFO XPM_MEMORY} [current_project]
-  add_files -quiet /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.runs/synth_1/arty_microblaze_01_wrapper.dcp
-  set_msg_config -source 4 -id {BD 41-1661} -limit 0
-  set_param project.isImplRun true
-  add_files /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.srcs/sources_1/bd/arty_microblaze_01/arty_microblaze_01.bd
-  set_param project.isImplRun false
-  read_xdc /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.srcs/constrs_1/new/constraints_arty.xdc
-  set_param project.isImplRun true
-  link_design -top arty_microblaze_01_wrapper -part xc7a35ticsg324-1L
-  set_param project.isImplRun false
-  write_hwdef -force -file arty_microblaze_01_wrapper.hwdef
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force arty_microblaze_01_wrapper_opt.dcp
-  create_report "impl_1_opt_report_drc_0" "report_drc -file arty_microblaze_01_wrapper_drc_opted.rpt -pb arty_microblaze_01_wrapper_drc_opted.pb -rpx arty_microblaze_01_wrapper_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
-    implement_debug_core 
-  } 
-  place_design 
-  write_checkpoint -force arty_microblaze_01_wrapper_placed.dcp
-  create_report "impl_1_place_report_io_0" "report_io -file arty_microblaze_01_wrapper_io_placed.rpt"
-  create_report "impl_1_place_report_utilization_0" "report_utilization -file arty_microblaze_01_wrapper_utilization_placed.rpt -pb arty_microblaze_01_wrapper_utilization_placed.pb"
-  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file arty_microblaze_01_wrapper_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force arty_microblaze_01_wrapper_routed.dcp
-  create_report "impl_1_route_report_drc_0" "report_drc -file arty_microblaze_01_wrapper_drc_routed.rpt -pb arty_microblaze_01_wrapper_drc_routed.pb -rpx arty_microblaze_01_wrapper_drc_routed.rpx"
-  create_report "impl_1_route_report_methodology_0" "report_methodology -file arty_microblaze_01_wrapper_methodology_drc_routed.rpt -pb arty_microblaze_01_wrapper_methodology_drc_routed.pb -rpx arty_microblaze_01_wrapper_methodology_drc_routed.rpx"
-  create_report "impl_1_route_report_power_0" "report_power -file arty_microblaze_01_wrapper_power_routed.rpt -pb arty_microblaze_01_wrapper_power_summary_routed.pb -rpx arty_microblaze_01_wrapper_power_routed.rpx"
-  create_report "impl_1_route_report_route_status_0" "report_route_status -file arty_microblaze_01_wrapper_route_status.rpt -pb arty_microblaze_01_wrapper_route_status.pb"
-  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file arty_microblaze_01_wrapper_timing_summary_routed.rpt -pb arty_microblaze_01_wrapper_timing_summary_routed.pb -rpx arty_microblaze_01_wrapper_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file arty_microblaze_01_wrapper_incremental_reuse_routed.rpt"
-  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file arty_microblaze_01_wrapper_clock_utilization_routed.rpt"
-  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file arty_microblaze_01_wrapper_bus_skew_routed.rpt -pb arty_microblaze_01_wrapper_bus_skew_routed.pb -rpx arty_microblaze_01_wrapper_bus_skew_routed.rpx"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force arty_microblaze_01_wrapper_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-  unset ACTIVE_STEP 
-}
-
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param chipscope.maxJobs 2
+  open_checkpoint arty_microblaze_01_wrapper_routed.dcp
+  set_property webtalk.parent_dir /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.cache/wt [current_project]
   set_property XPM_LIBRARIES {XPM_CDC XPM_FIFO XPM_MEMORY} [current_project]
+  add_files /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.srcs/sources_1/bd/arty_microblaze_01/ip/arty_microblaze_01_microblaze_0_0/data/mb_bootloop_le.elf
+  set_property SCOPED_TO_REF arty_microblaze_01 [get_files -all /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.srcs/sources_1/bd/arty_microblaze_01/ip/arty_microblaze_01_microblaze_0_0/data/mb_bootloop_le.elf]
+  set_property SCOPED_TO_CELLS microblaze_0 [get_files -all /home/teamradpc/Projects/CSCI_OS_Project/arty_linux.srcs/sources_1/bd/arty_microblaze_01/ip/arty_microblaze_01_microblaze_0_0/data/mb_bootloop_le.elf]
   catch { write_mem_info -force arty_microblaze_01_wrapper.mmi }
   catch { write_bmm -force arty_microblaze_01_wrapper_bd.bmm }
   write_bitstream -force arty_microblaze_01_wrapper.bit 
